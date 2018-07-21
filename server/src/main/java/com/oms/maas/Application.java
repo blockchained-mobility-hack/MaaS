779a70d8201d7bf65aede3f7566add2c63d7d00b
prunkton <prunkton@web.de>
2018-07-21 13:28:10 +0200

    introduce spring boot and swagger as our api framework (wip)

---
+++ server/src/main/java/com/oms/maas/Application.java
@@ -0,0 +1,20 @@
+package com.oms.maas;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class Application {
+
+
+    public Application() {
+        System.out.printf("breakpoint");
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+
+}
